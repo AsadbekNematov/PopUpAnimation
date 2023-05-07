19055cfc860223c07b6bd37b30b4e72e28368762
Asadbek <asadbekn443@gmail.com>
2023-05-07 05:10:17 -0400

    Initial Commit

---
+++ PopUpAnimation/PopUpAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopUpAnimationApp.swift
+//  PopUpAnimation
+//
+//  Created by Asadbek Nematov on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct PopUpAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
